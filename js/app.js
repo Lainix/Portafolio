85d99a990721be284ba5d1408724147351b06202
Lainix <linkelin8@gmail.com>
2018-09-03 21:19:58 -0500

    Portafolio 0.2

--- js/app.js
+++ js/app.js
@@ -1,5 +1,59 @@
 $(document).ready(function(){
 
+    $('a').click(function(){
+        var selected = $(this);
+        $('a').removeClass('active');
+        $(selected).addClass('active');
+    });
+
+    $a = $('.a'),
+    $b = $('.b'),
+    $c = $('.c'),
+    $d = $('.d'),
+    $e = $('.e'),
+    $inicio = $('.inicio'),
+    $about = $('.about'),
+    $skills = $('.skills'),
+    $portafolio = $('.portafolio'),
+    $contact = $('.contact'),
+
+    $a.click(function(){
+        $inicio.fadeIn();
+        $about.fadeOut();
+        $skills.fadeOut();
+        $portafolio.fadeOut();
+        $contact.fadeOut();
+    });
+    $b.click(function(){
+        $about.fadeIn();
+        $inicio.fadeOut();
+        $skills.fadeOut();
+        $portafolio.fadeOut();
+        $contact.fadeOut();
+    });
+    $c.click(function(){
+        $skills.fadeIn();
+        $inicio.fadeOut();
+        $about.fadeOut();
+        $portafolio.fadeOut();
+        $contact.fadeOut();
+    });
+    $d.click(function(){
+        $portafolio.fadeIn();
+        $inicio.fadeOut();
+        $about.fadeOut();
+        $skills.fadeOut();
+        $contact.fadeOut();
+    });
+    $e.click(function(){
+        $contact.fadeIn();
+        $inicio.fadeOut();
+        $about.fadeOut();
+        $skills.fadeOut();
+        $portafolio.fadeOut();
+    });
+
+
     $('#btnSend').click(function(){
 
         var errores = '';
